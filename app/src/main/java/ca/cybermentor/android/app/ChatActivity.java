ab096c49ad0f8443f09f4325fdc1ca85229bb143
Amit Uttam <changingrainbows@gmail.com>
2015-03-26 19:26:18 -0300

    Replace frame contents after a time lag

--- app/src/main/java/ca/cybermentor/android/app/ChatActivity.java
+++ app/src/main/java/ca/cybermentor/android/app/ChatActivity.java
@@ -1,18 +1,20 @@
 package ca.cybermentor.android.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.os.CountDownTimer;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class ChatActivity extends ActionBarActivity {
+
+    // Layout views
+    private ListView conversationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,27 @@ public class ChatActivity extends ActionBarActivity {
         setContentView(R.layout.activity_chat);
     }
 
+    protected void onStart() {
+        super.onStart();
+
+        final TextView messageView = (TextView) findViewById(R.id.message_entry);
+        String message = messageView.getText().toString();
+        Log.e("ZOSO", "++++ SLEEPING +++");
+
+        new CountDownTimer(3000, 1000) {
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            public void onFinish() {
+                FrameLayout frame = (FrameLayout) findViewById(R.id.conversation_frame);
+                frame.removeAllViews();
+                frame.addView(messageView);
+            }
+        }.start();
+
+        //ArrayAdapter conversationArrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_chat);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
