8c57338f821a9f86b838206798d78e42bbb557d4
Amit Uttam <changingrainbows@gmail.com>
2015-03-28 11:10:57 -0300

    Send button - basic wire up

--- app/src/main/java/ca/cybermentor/android/app/ChatActivity.java
+++ app/src/main/java/ca/cybermentor/android/app/ChatActivity.java
@@ -1,20 +1,22 @@
 package ca.cybermentor.android.app;
 
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-
 
 public class ChatActivity extends ActionBarActivity {
 
     // Layout views
     private ListView conversationView;
+    private TextView messageBox;
+    private String message;
+    private Button sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,24 +24,22 @@ public class ChatActivity extends ActionBarActivity {
         setContentView(R.layout.activity_chat);
     }
 
+    @Override
     protected void onStart() {
         super.onStart();
 
-        final TextView messageView = (TextView) findViewById(R.id.message_entry);
-        String message = messageView.getText().toString();
-        Log.e("ZOSO", "++++ SLEEPING +++");
+        messageBox = (TextView) findViewById(R.id.message_entry);
+        sendButton = (Button) findViewById(R.id.send_button);
 
-        new CountDownTimer(3000, 1000) {
-            public void onTick(long millisUntilFinished) {
+        final TextView chatView = (TextView) findViewById(R.id.scroll_chat);
 
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                message = messageBox.getText().toString();
+                chatView.setText(message);
             }
-
-            public void onFinish() {
-//                FrameLayout frame = (FrameLayout) findViewById(R.id.conversation_frame);
-//                frame.removeAllViews();
-//                frame.addView(messageView);
-            }
-        }.start();
+        }
+        );
 
         //ArrayAdapter conversationArrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_chat);
     }
