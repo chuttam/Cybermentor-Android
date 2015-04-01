a48729d2c27a9dd9819ec97b73fe9c3ec0caa902
Amit Uttam <changingrainbows@gmail.com>
2015-03-31 23:15:07 -0300

    Refactor out API service object

--- app/src/main/java/ca/cybermentor/android/app/api/CybermentorApi.java
+++ app/src/main/java/ca/cybermentor/android/app/api/CybermentorApi.java
@@ -21,27 +21,4 @@ public class CybermentorApi {
 
         service = restAdapter.create(Cybermentor.class);
     }
-
-    public String getMessageHistory(String uid1, String uid2) {
-
-        Callback callback =  new Callback<Response>() {
-            @Override
-            public void success(Response result, Response response) {
-                try {
-                    body = convertStreamToString(response.getBody().in());
-                } catch (IOException e) {}
-            }
-
-            @Override
-            public void failure(RetrofitError error) {}
-        };
-
-        service.messageHistory(uid1, uid2, callback);
-        return body;
-    }
-
-    public String convertStreamToString(InputStream is) {
-        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
-        return s.hasNext() ? s.next() : "";
-    }
 }
---
+++ app/src/main/java/ca/cybermentor/android/app/api/service/ConversationService.java
@@ -0,0 +1,47 @@
+package ca.cybermentor.android.app.api.service;
+
+import android.util.Log;
+
+import com.squareup.otto.Bus;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import ca.cybermentor.android.app.api.CybermentorApi;
+import ca.cybermentor.android.app.event.LoadConversationEvent;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class ConversationService {
+
+    final CybermentorApi cybermentorApi;
+    final Bus eventBus;
+
+    public ConversationService(CybermentorApi cybermentorApi, Bus eventBus) {
+        this.cybermentorApi = cybermentorApi;
+        this.eventBus = eventBus;
+    }
+
+    public void getMessageHistory(String uid1, String uid2) {
+
+        Callback callback =  new Callback<Response>() {
+            @Override
+            public void success(Response result, Response response) {
+                try {
+                    eventBus.post(new LoadConversationEvent(convertStreamToString(response.getBody().in()), response));
+                } catch (IOException e) {}
+            }
+
+            @Override
+            public void failure(RetrofitError error) {}
+        };
+
+        cybermentorApi.service.messageHistory(uid1, uid2, callback);
+    }
+
+    public String convertStreamToString(InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
+}
