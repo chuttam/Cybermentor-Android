e6883bcbb4a1d2739a1e18cfbfd6e4f167c8d485
Amit Uttam <changingrainbows@gmail.com>
2015-04-05 01:01:56 -0300

    Add todo - orientation testing

--- app/src/main/java/ca/cybermentor/android/app/activity/ChatActivity.java
+++ app/src/main/java/ca/cybermentor/android/app/activity/ChatActivity.java
@@ -147,6 +147,8 @@ public class ChatActivity extends ActionBarActivity {
     @Override
     public void onResume() {
         super.onResume();
+        // TODO: Works, but shouldn't have to reload conversation just for orientation change.
+        // Make current state known.
         eventBus.register(this);
     }
 
