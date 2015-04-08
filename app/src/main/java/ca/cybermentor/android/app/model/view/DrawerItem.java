1f2b8b43ffeea23accb0a1cf6ec82b137f21eb54
Amit Uttam <changingrainbows@gmail.com>
2015-04-08 02:01:57 -0300

    Model drawer list item with row layout

---
+++ app/src/main/java/ca/cybermentor/android/app/model/view/DrawerItem.java
@@ -0,0 +1,12 @@
+package ca.cybermentor.android.app.model.view;
+
+import ca.cybermentor.android.app.model.Participant;
+
+public class DrawerItem {
+
+    public Participant recipient;
+
+    public DrawerItem(Participant recipient) {
+        this.recipient = recipient;
+    }
+}
